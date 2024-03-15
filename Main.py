6792dc51173f7f76d51d5f772a4e91b025e05f4e
Chase Kew <jkew@google.com>
2024-03-14 18:59:19 -0700

    Stop button, pen up/down, spline w/ sync mode, no file writing when running plotter

--- Main.py
+++ Main.py
@@ -6,8 +6,6 @@ from absl import app
 from absl import flags
 import time
 import tkinter as tk
-from collections import deque
-import math
 import serial
 import codecs
 import numpy as np
@@ -23,6 +21,8 @@ SERIAL_PORT = flags.DEFINE_string(
 SYNC_MODE = True
 SPEED = 7000
 SEND_FREQUENCY = 20
+PEN_UP = (None, None)
+
 
 class GCodeSender:
     def __init__(self, serial_port):
@@ -45,6 +45,9 @@ class GCodeSender:
         print('Homing')
         self.send('$H\n')
 
+    def send_stop(self):
+        self.send('!')
+
     def reset_fluidnc(self):
         """Pulse the reset line for FluidNC"""
         print("Resetting FluidNC")
@@ -60,15 +63,25 @@ class GCodeSender:
             self.serial_instance.close()
 
 
+class GCodeFileWriter:
+    def __init__(self, filename='output.txt'):
+        self.filename = filename
+        # Clear file contents
+        open(self.filename, 'w').close()
+
+    def send(self, message):
+        with open(self.filename, 'a') as f:
+            f.write(message)
+
+
 def fit_bspline(points):
+    if not len(points):
+        return []
     x_coords, y_coords = zip(*points)
-    # TODO: Clean this up. (removing duplicate points)
-    points = np.array(points)[np.array(np.where(np.abs(np.diff(x_coords)) + np.abs(np.diff(y_coords)) > 0))].squeeze()
-    x_coords = points[:, 0]
-    y_coords = points[:, 1]
     tck, u = splprep([x_coords, y_coords], k=3)
-    bspline = splev(u[::5], tck)
+    bspline = splev(u[::3], tck)
     return np.array(bspline).T
+
 
 class DrawingApp:
     def __init__(self, root, gcode_sender):
@@ -95,32 +108,56 @@ class DrawingApp:
         
         # Button to home the machine
         self.home_button = tk.Button(root, text="Home Machine", command=self.home_machine)
-        self.home_button.pack(side=tk.LEFT, padx=(0, 20), pady=10)
+        self.home_button.pack(side=tk.LEFT, padx=(20, 20), pady=10)
 
         # Button to generate G-code
         self.generate_button = tk.Button(root, text="Generate G-code", command=self.generate_gcode)
         self.generate_button.pack(side=tk.LEFT, padx=(0, 20), pady=10)
 
         # Button to preview drawing for testing
-        self.preview_button = tk.Button(root, text="Preview Drawing", command=self.preview_drawing)
+        self.preview_button = tk.Button(root, text="Preview Drawing", command=self.preview_drawing,
+                                        state=tk.DISABLED if SYNC_MODE else tk.NORMAL)
         self.preview_button.pack(side=tk.LEFT, padx=(0, 20), pady=10)
 
         self.reset_button = tk.Button(root, text="Reset Plotter", command=self.reset_plotter)
         self.reset_button.pack(side=tk.LEFT, padx=(0, 20), pady=10)
 
+        self.pen_up_button = tk.Button(root, text="Pen up", command=self.raise_pen)
+        self.pen_up_button.pack(side=tk.LEFT, padx=(0, 20), pady=10)
+
+        self.pen_down_button = tk.Button(root, text="Pen down", command=self.lower_pen)
+        self.pen_down_button.pack(side=tk.LEFT, padx=(0, 20), pady=10)
+
+        self.stop_button = tk.Button(root, text="Stop", command=self.stop_plotter, fg='white', bg='red')
+        self.stop_button.pack(side=tk.LEFT, padx=(0, 20), pady=10)
+
     def home_machine(self):
         self.gcode_sender.send_homing_command()
+
+    def stop_plotter(self):
+        self.gcode_sender.send_stop()
         
     def reset_plotter(self):
         self.gcode_sender.reset_fluidnc()
         self.home_machine()
+
+    def raise_pen(self):
+        gcode = "G0 Z5\n"
+        self.gcode_sender.send(gcode)
+        self.pen_up = True
+
+    def lower_pen(self):
+        gcode = "G0 Z-5\n"
+        self.gcode_sender.send(gcode)
+        self.pen_up = False
 
     def setup(self):
         self.old_x = None
         self.old_y = None
         self.line_width = 2
         self.color = 'black'
-        self.positions = deque()  # Using deque for storing positions
+        self.positions = []
+        self.pen_up = True
 
         # Scaling factors
         self.x_scale = self.plotter_width / self.canvas_width
@@ -129,13 +166,11 @@ class DrawingApp:
     def is_within_canvas(self, x, y):
         return 0 <= x <= self.canvas.winfo_width() and 0 <= y <= self.canvas.winfo_height()
 
-    #Updated Draw Method to deal with polling issue
     def draw(self, event):
         if self.old_x == event.x and self.old_y == event.y:
             return  # splprep doesn't like repeating points
         if self.old_x and self.old_y and self.is_within_canvas(event.x, event.y):
-            # Interpolate points between the last position and the current position
-            self.interpolate_and_store(self.old_x, self.old_y, event.x, event.y)
+            self.positions.append((event.x, event.y))
 
             # Draw line
             self.canvas.create_line(self.old_x, self.old_y, event.x, event.y,
@@ -147,28 +182,11 @@ class DrawingApp:
         self.old_x = event.x
         self.old_y = event.y
 
-    def interpolate_and_store(self, x1, y1, x2, y2):
-        distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-        
-        # Define how many points based on the distance
-        points_count = max(int(distance / 2), 1)  # Ensure at least one point is added
-        # TODO: Maybe remove interpolation and explicitly mark pen up and pen down
-
-        for i in range(1, points_count + 1):
-            # Linear interpolation
-            x = x1 + (x2 - x1) * i / points_count
-            y = y1 + (y2 - y1) * i / points_count
-            self.positions.append((x, y))
-
     def reset(self, event):
         self.old_x = None
         self.old_y = None
+        self.positions.append(PEN_UP)
         
-    def preview_drawing(self):
-        bspline = fit_bspline(self.positions)
-        self._preview_drawing(bspline)
-        # self.plot_bspline()
-
     def plot_bspline(self, bspline):
         plt.plot(*zip(*self.positions), marker='o', linestyle='-', color='blue', label='Mouse Points')
         plt.plot(*zip(*bspline), marker='x', linestyle='-', color='red', label='B Spline')
@@ -176,76 +194,64 @@ class DrawingApp:
         plt.legend()
         plt.show()
 
-    def _preview_drawing(self, positions):
+    def preview_drawing(self):
         preview_window = tk.Toplevel(self.root)
         preview_window.title("Drawing Preview")
         
         preview_canvas = tk.Canvas(preview_window, bg='white', width=self.canvas_width, height=self.canvas_height)
         preview_canvas.pack()
 
+        preview_positions = self.positions.copy()
         last_x, last_y = None, None
-        deadzone = 10
-
-        for x, y in positions:
-
-            if last_x is not None and last_y is not None:
-                distance = math.sqrt((x - last_x) ** 2 + (y - last_y) ** 2)
-                if distance > deadzone:
-                    # Starting a new stroke, so lift the pen
-                    last_x, last_y = None, None  # Reset last positions to create a gap
-
-            if last_x is not None and last_y is not None:
-                # Draw line for continuous stroke
-                preview_canvas.create_line(last_x, last_y, x, y, width=self.line_width, fill=self.color)
-            else:
-                # Mark the start of a new stroke
-                preview_canvas.create_oval(x-2, y-2, x+2, y+2, fill=self.color)
-
-            last_x, last_y = x, y
-
-    def generate_gcode(self, filename='output.txt', deadzone=10):
-        start = time.time()
-        with open(filename, 'w') as file:
-            last_x, last_y = None, None
-            pen_up = True
-
-            positions = fit_bspline(self.positions)
+        while preview_positions:
+            if preview_positions[0] == PEN_UP:
+                last_x, last_y = None, None  # Reset last positions to create a gap
+                del preview_positions[0]
+            try:
+                pen_up_idx = preview_positions.index(PEN_UP)
+            except ValueError:
+                pen_up_idx = len(preview_positions)
+            positions = fit_bspline(preview_positions[:pen_up_idx])
+            preview_positions = preview_positions[pen_up_idx:]
             for x, y in positions:
-
                 if last_x is not None and last_y is not None:
-                    # Calculate distance to previous point
-                    distance = math.sqrt((x - last_x) ** 2 + (y - last_y) ** 2)
+                    # Draw line for continuous stroke
+                    preview_canvas.create_line(last_x, last_y, x, y, width=self.line_width, fill=self.color)
+                else:
+                    # Mark the start of a new stroke
+                    preview_canvas.create_oval(x-2, y-2, x+2, y+2, fill='red')
 
-                    if distance > deadzone and not pen_up:
-                        # Pen up command
-                        file.write("M0 ;Pen up\n")
-                        pen_up = True
-                
+                last_x, last_y = x, y
+
+    def generate_gcode(self):
+        start = time.time()
+        while self.positions:
+            if self.positions[0] == PEN_UP:
+                gcode = "G0 Z5\n"
+                self.pen_up = True
+                if self.gcode_sender:
+                    self.gcode_sender.send(gcode)
+                del self.positions[0]
+            try:
+                pen_up_idx = self.positions.index(PEN_UP)
+            except ValueError:
+                pen_up_idx = len(self.positions)
+            positions = fit_bspline(self.positions[:pen_up_idx])
+            self.positions = self.positions[pen_up_idx:]
+            for x, y in positions:
                 # Scale and round the coordinates to a resolution of 0.1mm
                 xScaled = round(self.x_scale * x, 1)
                 yScaled = round(self.y_scale * (self.canvas_height - y), 1)  # Flip the y coordinate
+                gcode = f"G1 X{xScaled} Y{yScaled} F{SPEED}\n"
 
-                if pen_up:
-                    # Move to new position with pen up
-                    file.write(f"G0 X{xScaled} Y{yScaled}\n")
-                    gcode = f"G1 X{xScaled} Y{yScaled} F{SPEED}\n"
-                    # Pen down command
-                    file.write("M1 ;Pen down\n")
-                    pen_up = False
-                else:
-                    # Move to new position with pen down
-                    file.write(f"G1 X{xScaled} Y{yScaled}\n")
-                    gcode = f"G1 X{xScaled} Y{yScaled} F{SPEED}\n"
+                if self.pen_up:
+                    gcode += "G0 Z-5\n"
+                    self.pen_up = False
+
                 if self.gcode_sender:
                     self.gcode_sender.send(gcode)
 
-                last_x, last_y = x, y
-            self.positions = deque()
-
-            # Ensure pen is up at the end
-            file.write("M5 ;Pen up\n")
-
-        print(f"G-code written to {filename} in {time.time() - start} s")
+        print(f"G-code written in {time.time() - start} s")
 
 def main(argv):
     del argv  # unused
@@ -253,7 +259,7 @@ def main(argv):
     if SERIAL_PORT.value and SERIAL_PORT.value.lower() != 'none':
         gcode_sender = GCodeSender(SERIAL_PORT.value)
     else:
-        gcode_sender = None
+        gcode_sender = GCodeFileWriter()
     app = DrawingApp(root, gcode_sender)
     root.mainloop()
 
